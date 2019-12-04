8905e8ca499afda82ec7755ce60b8550fb764511
Keith Galli <k.galli603@gmail.com>
2019-12-03 22:42:49 -0500

    Added shrink factor. Seeded it to a value that looked good

--- recursive_tree2.py
+++ recursive_tree2.py
@@ -1,35 +1,35 @@
 import turtle
 import random
 
+random.seed(46)
+
 BRANCH_COUNT = 7
 TURN_ANGLE = 10
+SHRINK_FACTOR = 0.8
 
 def tree(size, depth):
   if depth >= 1:
     if random.random() > 0.33:
-      turt.width(depth*1.5)
+      turt.width(depth)
       turt.color(get_color(depth))
       turt.forward(size)
       turt.right(TURN_ANGLE)
-      tree(size/1.25, depth-1)
+      tree(size*SHRINK_FACTOR, depth-1)
       turt.right(TURN_ANGLE)
-      tree(size/1.25, depth-1)
+      tree(size*SHRINK_FACTOR, depth-1)
       turt.left(3*TURN_ANGLE)
-      tree(size/1.25, depth-1)
+      tree(size*SHRINK_FACTOR, depth-1)
       turt.left(TURN_ANGLE)
-      tree(size/1.25, depth-1)
+      tree(size*SHRINK_FACTOR, depth-1)
       turt.right(2*TURN_ANGLE)
       turt.color(get_color(depth))
       turt.backward(size)
 
 def get_color(depth):
-  # Brown, Brown-Green, Green
   if BRANCH_COUNT - depth < 3:
     return 'brown'
-  elif depth < 3:
-    return 'green'
   else:
-    return '#74871c'
+    return 'green'
 
 if __name__ == '__main__':
   turt = turtle.Turtle()
